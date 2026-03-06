23d11e6dbc7b06e5a257f7ee6b26a93a3b559484
Will <416838606@qq.com>
2026-03-06 15:17:19 +0800

    update

--- app/(app)/activity/page.tsx
+++ app/(app)/activity/page.tsx
@@ -75,16 +75,15 @@ const ActivityPage: FC = () => {
             }}
             onClick={() => goActivityDetail(item.id)}
           >
-            <div>
               {/* 模糊背景层 */}
-              <div
+              {/* <div
                 className="absolute inset-0 bg-cover bg-no-repeat bg-center blur-2xl scale-110 -z-10"
                 style={{ backgroundImage: `url(/images/activity/${item.type}.png)` }}
-              />
+              /> */}
               {/* 亮色主题渐变覆盖层 */}
-              <div className="absolute inset-0 bg-linear-to-r from-background/90 via-background/30 to-transparent md:from-background/60 md:via-background/10" />
+              {/* <div className="absolute inset-0 bg-linear-to-r from-background/90 via-background/30 to-transparent md:from-background/60 md:via-background/10" /> */}
               {/* 暗色主题渐变覆盖层 */}
-              <div className="absolute inset-0 bg-linear-to-r dark:from-foreground/80 dark:via-foreground/20 dark:to-transparent md:dark:from-foreground/40 md:dark:via-foreground/5" />
+              {/* <div className="absolute inset-0 bg-linear-to-r dark:from-foreground/80 dark:via-foreground/20 dark:to-transparent md:dark:from-foreground/40 md:dark:via-foreground/5" /> */}
               <CardHeader className={`${getTextColor(item.type)} ${getDarkTextColor(item.type)} opacity-80`}>
                 <h2 className="text-lg md:text-xl truncate">
                   {item.title}
@@ -110,7 +109,6 @@ const ActivityPage: FC = () => {
                   reloadActivity={reloadActivity}
                 />
               </CardFooter>
-            </div>
           </Card>
         ))
       }
--- components/web/deleteDialog.tsx
+++ components/web/deleteDialog.tsx
@@ -1,24 +1,29 @@
 'use client'
-import { FC } from "react";
+import React, { FC } from "react";
 import { AlertDialogHeader, AlertDialogFooter, AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogCancel, AlertDialogAction } from "../ui/alert-dialog"
 import { Button } from "../ui/button"
 import { deleteById } from "@/app/(app)/activity/actions/activityActions";
 
 const DeleteDialog: FC<{ id: string, title: string, reloadActivity: () => void }> = ({ id, title, reloadActivity }) => {
 
-    const deleteActivity = async () => {
+    const deleteActivity = async (event:React.MouseEvent) => {
+        stopPopup(event);
         const actionResult = await deleteById('activity', id);
         if (actionResult) {
             reloadActivity()
         }
     }
 
+    const stopPopup = (event:React.MouseEvent) => {
+        event.stopPropagation();
+    }
+
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <Button variant="destructive" onClick={(e) => {e.stopPropagation();}}>删除</Button>
+                <Button variant="destructive" onClick={stopPopup}>删除</Button>
             </AlertDialogTrigger>
-            <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+            <AlertDialogContent>
                 <AlertDialogHeader>
                     <AlertDialogTitle>确定删除活动：{title}?</AlertDialogTitle>
                     <AlertDialogDescription>
@@ -26,7 +31,7 @@ const DeleteDialog: FC<{ id: string, title: string, reloadActivity: () => void }
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogCancel onClick={stopPopup}>Cancel</AlertDialogCancel>
                     <AlertDialogAction variant="destructive" onClick={deleteActivity}>Continue</AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
