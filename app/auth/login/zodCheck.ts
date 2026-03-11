42bb984fa3eb0ac1f9ec66763e92b96d80aebe93
Will <416838606@qq.com>
2026-03-11 15:36:14 +0800

    hono + openapi

--- app/auth/login/zodCheck.ts
+++ app/auth/login/zodCheck.ts
@@ -16,19 +16,19 @@ export const signUpCheck = z.object({
 })
 
 export const editActivityCheck = z.object({
-    title: z.string(), // 改为title而不是name
-    start_time: z.string().optional().refine(
+    title: z.string().describe('活动标题'), // 改为title而不是name
+    start_time: z.string().describe('活动开始时间').optional().refine(
         (val) => !val || !isNaN(Date.parse(val)),
         {message: "Invalid date format"}
     ),
-    end_time: z.string().optional().refine(
+    end_time: z.string().describe('活动结束时间').optional().refine(
         (val) => !val || !isNaN(Date.parse(val)),
         {message: "Invalid date format"}
     ),
-    type: z.enum(ActivityTypes).optional(), // 改为可选
-    status: z.enum(ActivityStatus).optional(), // 改为可选
-    desc: z.string().optional(), // 改为可选
-    author: z.string().optional(), // 改为可选
-    content: z.string().optional(), // 改为可选
+    type: z.enum(ActivityTypes).describe('活动类型').optional(), // 改为可选
+    status: z.enum(ActivityStatus).describe('活动状态').optional(), // 改为可选
+    desc: z.string().optional().describe('描述信息'), // 改为可选
+    author: z.string().describe('创建人').optional(), // 改为可选
+    content: z.string().describe('活动说明').optional(), // 改为可选
     id: z.string()
 })
